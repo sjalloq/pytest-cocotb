1915d048593eaecc8d1b5b37cbe43a298569ad35
Shareef Jalloq <shareef@jalloq.co.uk>
2026-02-27 19:21:33 +0000

    Use dynamic versioning

--- src/pytest_cocotb/__init__.py
+++ src/pytest_cocotb/__init__.py
@@ -2,4 +2,9 @@
 
 from .session import TestSession
 
-__all__ = ["TestSession"]
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    __version__ = "0.0.0.dev0+unknown"
+
+__all__ = ["TestSession", "__version__"]
